99a51a772576422328632262ffdb6800a2b57366
Damola Adediran <tylher123@gmail.com>
2022-02-28 19:21:03 +0100

    Add javascript linter

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-let body = document.querySelector("body");
-
+const body = document.querySelector('body');
 function displayNav() {
-  document.body.getElementsByClassName("header-not-visible")[0].style.display =
-    "none";
-  document.body.getElementsByClassName("mobile-menu")[0].style.display =
-    "block";
-  body.style.height = "100vh";
-  body.style.overflowY = "hidden";
+  document.body.getElementsByClassName('header-not-visible')[0].style.display = 'none';
+  document.body.getElementsByClassName('mobile-menu')[0].style.display = 'block';
+  body.style.height = '100vh';
+  body.style.overflowY = 'hidden';
 }
 
 function closeNav() {
-  document.body.getElementsByClassName("header-not-visible")[0].style.display =
-    "flex";
-  document.body.getElementsByClassName("mobile-menu")[0].style.display = "none";
-  body.style.height = "auto";
-  body.style.overflowY = "scroll";
+  document.body.getElementsByClassName('header-not-visible')[0].style.display = 'flex';
+  document.body.getElementsByClassName('mobile-menu')[0].style.display = 'none';
+  body.style.height = 'auto';
+  body.style.overflowY = 'scroll';
 }
