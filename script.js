769a57050e223a20b1b5af1a36aa753644abd3d6
Damola Adediran <tylher123@gmail.com>
2022-02-28 16:54:07 +0100

    Update javascript file

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+let body = document.querySelector("body");
+
 function displayNav() {
-  document.body.getElementsByClassName('header-not-visible')[0].style.display = 'none'
-  document.body.getElementsByClassName('mobile-menu')[0].style.display = 'block'
+  document.body.getElementsByClassName("header-not-visible")[0].style.display =
+    "none";
+  document.body.getElementsByClassName("mobile-menu")[0].style.display =
+    "block";
+  body.style.height = "100vh";
+  body.style.overflowY = "hidden";
 }
 
-function closeNav(){
-  document.body.getElementsByClassName('header-not-visible')[0].style.display = 'flex'
-  document.body.getElementsByClassName('mobile-menu')[0].style.display = 'none'
+function closeNav() {
+  document.body.getElementsByClassName("header-not-visible")[0].style.display =
+    "flex";
+  document.body.getElementsByClassName("mobile-menu")[0].style.display = "none";
+  body.style.height = "auto";
+  body.style.overflowY = "scroll";
 }
