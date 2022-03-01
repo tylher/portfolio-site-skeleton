2fa65fda53aa65f79aa98a7f9edd529ac51e60eb
Damola Adediran <tylher123@gmail.com>
2022-03-01 20:44:52 +0100

    Create javascript file for data| Update script.js

---
+++ data.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export let data = {
+  /* eslint-enable */
+  project1: {
+    name: '',
+    description: '',
+    imageUrl: '',
+    tecnologies: [],
+    ProjectUrls: {
+      seeLive: '',
+      seeSource: '',
+    },
+  },
+  project2: {},
+  project3: {},
+};
--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+import { data } from './data.js';
+
 const body = document.querySelector('body');
+const seeProject = document.querySelectorAll('.see-project');
+const projectTitle = document.querySelector('.modal-text-box h2');
+const projectDescription = document.querySelector('.modal-text-box p');
+const projectImage = document.querySelectorAll('.modal img');
+const tecnologiesArr = document.querySelectorAll('.modal-tech li');
+const buttons = document.querySelectorAll('btn-group a');
+
 /* eslint-disable */
 const displayNav = () => {
-/* eslint-enable */
+  /* eslint-enable */
   document.body.getElementsByClassName('header-not-visible')[0].style.display = 'none';
   document.body.getElementsByClassName('mobile-menu')[0].style.display = 'block';
   body.style.height = '100vh';
@@ -9,10 +18,23 @@ const displayNav = () => {
 };
 /* eslint-disable */
 const closeNav = () => {
-/* eslint-enable */
+  /* eslint-enable */
   document.body.getElementsByClassName('header-not-visible')[0].style.display = 'flex';
   document.body.getElementsByClassName('mobile-menu')[0].style.display = 'none';
   body.style.height = 'auto';
   body.style.overflowY = 'scroll';
 };
 
+seeProject.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    document.querySelector('.container').style.display = 'none';
+    const Description = btn.parentElement.childNodes[1].textContent;
+    const projects = Object.keys(data);
+    projects.map((project) => {
+      if (data[project].description === Description) {
+
+      }
+    });
+    document.querySelector('.modal').style.display = 'block';
+  });
+});
