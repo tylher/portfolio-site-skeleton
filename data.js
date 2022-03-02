78a3e84110f5af01a3df628598419a83276abf16
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-03-02 18:15:08 +0100

    Construct data for project pop-up

--- data.js
+++ data.js
@@ -1,16 +1,70 @@
 /* eslint-disable */
-export let data = {
+export let data = [
   /* eslint-enable */
-  project1: {
-    name: '',
-    description: '',
-    imageUrl: '',
-    tecnologies: [],
+  {
+    name: "Keeping track of hundreds of components",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    imageUrl: "images/Snapshoot-Portfolio.svg",
+    tecnologies: ["Ruby on rails", "css", "JavScript"],
     ProjectUrls: {
-      seeLive: '',
-      seeSource: '',
+      seeLive: "https://tylher.github.io/portfolio-site-skeleton/",
+      seeSource: "https://github.com/tylher/portfolio-site-skeleton",
     },
   },
-  project2: {},
-  project3: {},
-};
+  {
+    name: "Multi-Post Stories Gain+Glory",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    imageUrl: "images/Snapshoot-Portfolio.svg",
+    tecnologies: ["Ruby on rails", "css", "JavScript"],
+    ProjectUrls: {
+      seeLive: "https://tylher.github.io/portfolio-site-skeleton/",
+      seeSource: "https://github.com/tylher/portfolio-site-skeleton",
+    },
+  },
+  {
+    name: "Keeping track of hundreds of components",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    imageUrl: "images/Snapshoot-Portfolio.svg",
+    tecnologies: ["Ruby on rails", "css", "JavScript"],
+    ProjectUrls: {
+      seeLive: "https://tylher.github.io/portfolio-site-skeleton/",
+      seeSource: "https://github.com/tylher/portfolio-site-skeleton",
+    },
+  },
+  {
+    name: "Multi-Post Stories Gain+Glory",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    imageUrl: "images/Snapshoot-Portfolio.svg",
+    tecnologies: ["Ruby on rails", "css", "JavScript"],
+    ProjectUrls: {
+      seeLive: "https://tylher.github.io/portfolio-site-skeleton/",
+      seeSource: "https://github.com/tylher/portfolio-site-skeleton",
+    },
+  },
+  {
+    name: "Keeping track of hundreds of components",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    imageUrl: "images/Snapshoot-Portfolio.svg",
+    tecnologies: ["Ruby on rails", "css", "JavScript"],
+    ProjectUrls: {
+      seeLive: "https://tylher.github.io/portfolio-site-skeleton/",
+      seeSource: "https://github.com/tylher/portfolio-site-skeleton",
+    },
+  },
+  {
+    name: "Keeping track of hundreds of components",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    imageUrl: "images/Snapshoot-Portfolio.svg",
+    tecnologies: ["Ruby on rails", "css", "JavScript"],
+    ProjectUrls: {
+      seeLive: "https://tylher.github.io/portfolio-site-skeleton/",
+      seeSource: "https://github.com/tylher/portfolio-site-skeleton",
+    },
+  },
+];
