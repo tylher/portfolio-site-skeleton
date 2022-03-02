34edc144b606e6fcc244ef519857c9604e49f68a
Damola Adediran <tylher123@gmail.com>
2022-03-02 18:31:00 +0100

    Make changes to the javascipt files

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ import { data } from './data.js';
 
 const body = document.querySelector('body');
 const seeProject = document.querySelectorAll('.see-project');
+const goBack = document.querySelector('.cancel-icon-desktop');
 const projectTitle = document.querySelector('.modal-text-box h2');
 const projectDescription = document.querySelector('.modal-text-box p');
 const projectImage = document.querySelectorAll('.modal img');
@@ -27,14 +28,18 @@ const closeNav = () => {
 
 seeProject.forEach((btn) => {
   btn.addEventListener('click', () => {
-    document.querySelector('.container').style.display = 'none';
+    document.querySelector('.container').classList.toggle('blur');
     const Description = btn.parentElement.childNodes[1].textContent;
-    const projects = Object.keys(data);
-    projects.map((project) => {
-      if (data[project].description === Description) {
+    data.map((project) => {
+      if (project.description === Description) {
 
       }
     });
-    document.querySelector('.modal').style.display = 'block';
+    document.querySelector('.modal-wrapper').style.display = 'flex';
   });
+});
+
+goBack.addEventListener('click', () => {
+  document.querySelector('.container').classList.toggle('blur');
+  document.querySelector('.modal-wrapper').style.display = 'none';
 });
