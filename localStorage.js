05b0894a1dcfac6ff2e7f437f6789633d7737b98
Damola Adediran <tylher123@gmail.com>
2022-03-04 15:17:42 +0100

    Fix hamburger issue

--- localStorage.js
+++ localStorage.js
@@ -1,31 +1,31 @@
 const fullName = document.getElementById('name');
-const email = document.getElementById('Email');
-const form = document.querySelector('.contact-us');
+const Email = document.getElementById('Email');
+const Form = document.querySelector('.contact-us');
 const reset = document.querySelector('.reset-btn');
-const errorMsg = document.querySelector('.error-msg');
+const ErrorMsg = document.querySelector('.error-msg');
 
 const storeUserData = () => {
   const data = {
     userName: fullName.value,
-    userEmail: email.value,
+    userEmail: Email.value,
   };
   localStorage.setItem('userData', JSON.stringify(data));
 };
 
 const getUserData = () => JSON.parse(localStorage.getItem('userData'));
 
-form.addEventListener('focusout', () => {
+Form.addEventListener('focusout', () => {
   storeUserData();
 });
 
 reset.addEventListener('click', (e) => {
   e.preventDefault();
-  email.value = '';
+  Email.value = '';
   fullName.value = '';
   storeUserData();
-  errorMsg.style.display = 'none';
+  ErrorMsg.style.display = 'none';
 });
 
 const dataObj = getUserData();
 fullName.value = dataObj.userName;
-email.value = dataObj.userEmail;
+Email.value = dataObj.userEmail;
--- script.js
+++ script.js
@@ -1,4 +1,71 @@
-import { data } from './data.js';
+const data = [
+  {
+    name: 'Events price calculator',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    imageUrl: 'images/Snapshoot-Portfolio.svg',
+    tecnologies: ['Ruby on rails', 'css', 'JavScript'],
+    ProjectUrls: {
+      seeLive: 'https://tylher.github.io/portfolio-site-skeleton/',
+      seeSource: 'https://github.com/tylher/portfolio-site-skeleton',
+    },
+  },
+  {
+    name: 'Reaching for the stars',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    imageUrl: './images/Snapshoot-Portfolio.svg',
+    tecnologies: ['Ruby on rails', 'css', 'JavScript'],
+    ProjectUrls: {
+      seeLive: 'https://tylher.github.io/portfolio-site-skeleton/',
+      seeSource: 'https://github.com/tylher/portfolio-site-skeleton',
+    },
+  },
+  {
+    name: 'Technology meetup website',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    imageUrl: 'images/Snapshoot-Portfolio.svg',
+    tecnologies: ['Ruby on rails', 'css', 'JavScript'],
+    ProjectUrls: {
+      seeLive: 'https://tylher.github.io/portfolio-site-skeleton/',
+      seeSource: 'https://github.com/tylher/portfolio-site-skeleton',
+    },
+  },
+  {
+    name: 'Multi-Post Stories Gain+Glory',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    imageUrl: 'images/Snapshoot-Portfolio.svg',
+    tecnologies: ['Ruby on rails', 'css', 'JavScript'],
+    ProjectUrls: {
+      seeLive: 'https://tylher.github.io/portfolio-site-skeleton/',
+      seeSource: 'https://github.com/tylher/portfolio-site-skeleton',
+    },
+  },
+  {
+    name: 'Keeping track of hundreds of components',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    imageUrl: 'images/Snapshoot-Portfolio.svg',
+    tecnologies: ['Ruby on rails', 'css', 'JavScript'],
+    ProjectUrls: {
+      seeLive: 'https://tylher.github.io/portfolio-site-skeleton/',
+      seeSource: 'https://github.com/tylher/portfolio-site-skeleton',
+    },
+  },
+  {
+    name: 'Daily Task Saver',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea",
+    imageUrl: 'images/Snapshoot-Portfolio.svg',
+    tecnologies: ['Ruby on rails', 'css', 'JavScript'],
+    ProjectUrls: {
+      seeLive: 'https://tylher.github.io/portfolio-site-skeleton/',
+      seeSource: 'https://github.com/tylher/portfolio-site-skeleton',
+    },
+  },
+];
 
 const body = document.querySelector('body');
 const seeProject = document.querySelectorAll('.see-project');
@@ -74,7 +141,7 @@ form.addEventListener("submit", (e) => {
   if (reg1.test(email.value.trim())&& !reg2.test(email.value.trim()) == false) {
     e.preventDefault();
     errorMsg.textContent = "Your email should be in lowercase";
-    errorMsg.style.display = "inline";
+    errorMsg.style.display = "inline-flex";
     email.value = email.value.toLowerCase();
   }
 });
